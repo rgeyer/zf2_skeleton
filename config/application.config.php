40c815913086422fc11f802cedcfee36391f114a
Ryan J. Geyer <me@ryangeyer.com>
2013-02-11 20:48:58 -0800

    Added zend developer tools

--- config/application.config.php
+++ config/application.config.php
@@ -4,7 +4,8 @@ return array(
     'modules' => array(
         'Application',
         'DoctrineModule',
-        'DoctrineORMModule'
+        'DoctrineORMModule',
+        'ZendDeveloperTools'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
